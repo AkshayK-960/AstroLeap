fe2047f84c683a184118e16fb0f8579a2e031f9a
AkshayK-960 <157665344+AkshayK-960@users.noreply.github.com>
2026-06-29 14:10:27 -0500

    add web build

---
+++ platform.gd
@@ -0,0 +1,15 @@
+extends StaticBody2D
+@export_enum("Normal", "Icy", "Fragile") var platform_type: String = "Normal"
+
+
+func _ready() -> void:
+	remove_from_group("Player")
+
+func handle_touch(player):
+	if platform_type == "Fragile":
+		await get_tree().create_timer(0.1).timeout
+		queue_free()
+	elif platform_type == "Icy":
+		player.is_slippery = true
+	elif platform_type == "Normal":
+		player.is_slippery = false
---
+++ score.gd
@@ -0,0 +1,24 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if label_settings == null:
+		label_settings = LabelSettings.new()
+	
+	
+	label_settings.font_size = 300
+	label_settings.font_color = Color(1.0, 1.0, 1.0, 1.0)
+
+var maxScore: int = 0
+func _process(_delta: float) -> void:
+	
+	
+	
+	var currScore = max(int(0), int(floor(-%Player.position.y / 100)))
+	
+	if currScore > maxScore:
+		maxScore = currScore
+		
+	self.text = str(maxScore)
+# text = str(int(floor(-%Player.position.y / 100)))
